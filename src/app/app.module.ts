df28da32032393de3b3ec7d5964bfe679c8574c5
Ricardo Lira <ricardo.19.rl09@gmail.com>
2018-04-23 10:21:55 -0500

    Servicio creado y checado(footer y header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpModule } from "@angular/http";
+
 //RUTAS
 
 import { app_routing } from "./app.routes";
+
+//SERVICIOS
+
+import { InformacionService } from "./services/informacion.service";
 
 //Components
 import { AppComponent } from './app.component';
@@ -12,6 +18,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { AboutComponent } from './components/about/about.component';
 import { PortafolioItemComponent } from './components/portafolio-item/portafolio-item.component';
+import { importExpr } from '@angular/compiler/src/output/output_ast';
 
 
 @NgModule({
@@ -22,12 +29,15 @@ import { PortafolioItemComponent } from './components/portafolio-item/portafolio
     PortafolioComponent,
     AboutComponent,
     PortafolioItemComponent
+    
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     app_routing
+   
   ],
-  providers: [],
+  providers: [InformacionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
