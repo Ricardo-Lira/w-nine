492202929187803151d4f9d7c733b4d4464893df
Ricardo Lira <ricardo.19.rl09@gmail.com>
2018-04-23 09:14:56 -0500

    Agregamos modificaciones del menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//RUTAS
 
+import { app_routing } from "./app.routes";
+
+//Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { AboutComponent } from './components/about/about.component';
+import { PortafolioItemComponent } from './components/portafolio-item/portafolio-item.component';
 
 
 @NgModule({
@@ -15,10 +20,12 @@ import { AboutComponent } from './components/about/about.component';
     HeaderComponent,
     FooterComponent,
     PortafolioComponent,
-    AboutComponent
+    AboutComponent,
+    PortafolioItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from "@angular/router";
+
+import { AboutComponent, PortafolioComponent, PortafolioItemComponent } from "./components/index.paginas";
+
+
+
+const app_routes: Routes =[
+
+    { path: 'home', component: PortafolioComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'PortafolioItemComponent', component: PortafolioItemComponent},
+    
+    { path: '**', pathMatch: 'full', redirectTo: 'home'}
+
+];
+
+export const app_routing = RouterModule.forRoot(app_routes, { useHash: true });
